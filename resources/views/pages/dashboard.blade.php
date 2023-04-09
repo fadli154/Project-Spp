fd473f96a89900cf769689c1447e409f49184ff2
fadli154 <fadlihifziansyah154@gmail.com>
2023-04-09 14:58:10 +0700

    Chore: update dashboard.blade.php change title

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -53,7 +53,7 @@
                             </div>
                             <div class="card-wrap">
                                 <div class="card-header">
-                                    <h4>Total Tagihan</h4>
+                                    <h4>Sisa Tagihan</h4>
                                 </div>
                                 <div class="card-body">
                                     @if ($totalTagihan != null)
